7bcd94bd40c1c149df57a2fd54d3ff6d1b97fbaf
Bokhanchenko <Bokhanchenko.dm@gmail.com>
2015-10-06 21:22:02 +0300

    Update TaskThree.java

--- oksana/src/main/hw/hw2/TaskThree.java
+++ oksana/src/main/hw/hw2/TaskThree.java
@@ -14,7 +14,7 @@ public class TaskThree {
         int b = 4;
         int c = 3;
         int e = 2;
-        double d = a + b/c/e;
+        double d = a + b/c/(float)e; // я не можу це пояснити)), мабуть змінено лише один тип змінної...
         System.out.println(d);
     }
 }
